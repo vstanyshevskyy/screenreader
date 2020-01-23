1fd533149e4917c2f5a778d7d754608d9e7c5e1e
Vitaliy Stanyshevskyy <vitaliy.stanyshevskyy@gmail.com>
2020-01-24 00:09:38 +0200

    Cache requests

--- src/js/content/tts/GoogleSpeechApiTts.js
+++ src/js/content/tts/GoogleSpeechApiTts.js
@@ -5,7 +5,6 @@ class GoogleSpeechApiTts extends BaseTts {
     super(...args);
     chrome.storage.sync.get(['api_key'], ({ api_key = "" }) => {
       this.apiKey = api_key;
-      console.log(this.apiKey);
     });
 
     this.lang = lang;
@@ -28,20 +27,17 @@ class GoogleSpeechApiTts extends BaseTts {
     if (!this.isEnabled) {
       return;
     }
-    const myHeaders = new Headers();
 
     const ttsReq = new Request(`https://texttospeech.googleapis.com/v1/text:synthesize?key=${this.apiKey}`, {
       method: 'POST',
-      headers: myHeaders,
-      mode: 'cors',
-      cache: 'default',
+      headers: new Headers([
+        ['cache-control', 'force-cache']
+      ]),
+      cache: 'force-cache',
       body: JSON.stringify({
-        input: {
-          text
-        },
+        input: { text },
         voice: {
           languageCode: this.lang,
-            // name: ,
           ssmlGender: "FEMALE"
         },
         audioConfig: {
@@ -53,8 +49,8 @@ class GoogleSpeechApiTts extends BaseTts {
     });
 
     fetch(ttsReq)
-      .then((response) => response.json())
-      .then((json) => {
+      .then(response => response.json())
+      .then(json => {
         this.player.src = `data:audio/mp3;base64,${json.audioContent}`;
         this.player.play();
       });
